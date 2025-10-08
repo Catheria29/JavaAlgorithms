6c106ebfe99501006ab35b4aac5e4751ad67030a
catheriabara <baracatheriaclaire@gmail.com>
2025-10-08 15:33:40 +0300

    Learned how to create an arraylist that contains many dataTypes

---
+++ src/MultipleDataTypesArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MultipleDataTypesArrayList {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<>();
+        myList.add(12);
+        myList.add(5);
+        myList.add("Catheria");
+        myList.add(5.67435345);
+        myList.add(3);
+        myList.add(16);
+        System.out.println(myList);
+    }
+}
